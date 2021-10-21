c73c43e14edc60e2409c7cdcf52fa156ab2f3463
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2021-10-22 02:21:35 +0600

    Add files via upload

---
+++ Watermelon.cpp
@@ -0,0 +1,40 @@
+// Bismillahir rahmanir rahim
+// Arman Hossain C193033
+// Programming is nothing but an addiction.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define sortv sort(v.begin(), v.end())
+#define rev_sortv sort(v.begin(), v.end(), greater<int>())
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vc = vector<char>;
+using vs = vector<string>;
+
+void input_output()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+void lets_code()
+{
+    int a;
+    cin >> a;
+    if (a % 2 == 1 || a == 2)
+        cout << "NO";
+    else
+        cout << "YES";
+}
+int main()
+{
+
+    input_output();
+
+    lets_code();
+
+    return 0;
+}
