0d0acc0c5b850c619fdc0c39941c3b29017fe22b
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-05-13 02:15:47 +0600

    Create Choose Elements.cpp
    https://codeforces.com/group/MWSDmqGsZm/contest/329103/problem/C

---
+++ Choose Elements.cpp
@@ -0,0 +1,51 @@
+
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+
+
+#define     pb              push_back
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(int i = 0; i < v.size(); ++i){cout << v[i] << " ";}cout << endl;
+#define     sz(s)           (s).length()
+
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:/io/input.txt", "r", stdin);
+    freopen("F:/io/output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n , k;
+    cin >> n >> k;
+    inv;
+    rev_sortv;
+    ll sum = 0;
+    //out(v);
+    for (int i = 0; i < k; ++i)
+    {
+        if(v[i] > 0)
+            sum+=v[i];
+    }
+    
+    cout << sum;
+    return 0;
+}
