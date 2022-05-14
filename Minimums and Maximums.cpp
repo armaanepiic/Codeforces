c8a6869f3ed91f85da7cd2edfb43808a6edd79b7
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-05-14 21:51:10 +0600

    Create Minimums and Maximums.cpp

---
+++ Minimums and Maximums.cpp
@@ -0,0 +1,57 @@
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+
+
+#define     pb              push_back
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(int i = 0; i < v.size(); ++i){cout << v[i] << " ";}cout << endl;
+#define     sz(s)           (s).length()
+
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:/io/input.txt", "r", stdin);
+    freopen("F:/io/output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin >> t;
+    while (t--) {
+        
+        bool flag = true;
+
+        int a , b , c , d;
+        cin >> a >> b >> c >> d;
+
+        int x;
+        x = max(a , c);
+        if((x == c && b < x) || (x == a && d < x))
+            cout << a+c << endl;
+        else
+            cout << x << endl;
+
+
+    }
+
+
+
+    return 0;
+}
