b62487aaecfeccfa8f8f6e33764a1e4f43410847
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-05-22 01:08:51 +0600

    Create Dictionary.cpp

---
+++ Dictionary.cpp
@@ -0,0 +1,61 @@
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+
+const int N = 1e8+10;
+int arr[N];
+
+
+#define     pb              push_back
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(int i = 0; i < v.size(); ++i){cout << v[i] << " ";}cout << endl;
+#define     sz(s)           (s).length()
+#define     all(x)          (x).begin(), (x).end()
+#define     digits(x)       trunc(log10(x))+1
+
+
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:/io/input.txt", "r", stdin);
+    freopen("F:/io/output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin >> t;
+    while (t--) {
+        
+        string s;
+        cin >> s;
+        int a , b;
+        a = s[0] - 97;
+        if(s[1] < s[0])
+            b = s[1] - 96;
+        else if(s[1] > s[0])
+            b = s[1] - 97;
+        
+        cout << a*25 + b << endl;
+ 
+    }
+
+
+
+    return 0;
+}
