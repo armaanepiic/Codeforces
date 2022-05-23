9ed383570a283fcf5d85d77a4d65988e1fdb6b10
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-05-24 00:10:30 +0600

    Create Card Trick.cpp

---
+++ Card Trick.cpp
@@ -0,0 +1,67 @@
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+
+
+
+
+#define     pb              push_back
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(int i = 0; i < v.size(); ++i){cout << v[i] << " ";}cout << endl;
+#define     sz(s)           (s).length()
+#define     all(x)          (x).begin(), (x).end()
+#define     digits(x)       trunc(log10(x))+1
+
+
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:/io/input.txt", "r", stdin);
+    freopen("F:/io/output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin >> t;
+    while (t--) {
+
+        int n; cin >> n;
+        vi v;
+        for (int i = 0; i < n; ++i)
+        {
+            int x; cin >> x;
+            v.pb(x);
+        }
+        int m; cin >> m;
+        ll sum = 0;
+        for (int i = 0; i < m; ++i)
+        {
+            int x; cin >> x;
+            sum += x;
+        }
+
+        int temp = sum % n;
+        cout << v[temp] << endl;
+        
+    }
+
+
+
+    return 0;
+}
