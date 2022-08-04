7b862ed00c44c52ed6ad8a732f9b27da3160b552
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-08-05 01:32:55 +0600

    Create Permutation Chain.cpp

---
+++ Permutation Chain.cpp
@@ -0,0 +1,76 @@
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+
+
+
+
+#define     pb              push_back
+#define     mp              make_pair
+#define     ff              first
+#define     ss              second
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(int i = 0; i < v.size(); ++i){cout << v[i] << " ";}cout << endl;
+#define     sz(s)           (s).length()
+#define     all(x)          (x).begin(), (x).end()
+#define     digits(x)       trunc(log10(x))+1
+#define     even(x)         (x)%2
+#define     inn             int n;cin>>n;
+
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:/io/input.txt", "r", stdin);
+    freopen("F:/io/output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin >> t;
+    while (t--) {
+        
+        bool flag = true;
+        ll count = 0;
+
+        int n;
+        cin >> n;
+        vi v;
+        for (int i = 1; i <= n; ++i)
+        {
+            v.pb(i);
+        }
+        cout << n << endl;
+        for (int i = 1; i <= n; ++i)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+        for (int i = 1; i < n; ++i)
+        {
+            swap(v[i],v[i-1]);
+            out(v);
+        }
+        
+
+    }
+
+
+
+    return 0;
+}
