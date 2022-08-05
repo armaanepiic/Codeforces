ccbff5b957e2f839396cc60ad21623ab6c9e20b6
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-08-05 18:12:37 +0600

    Create cAPS lOCK.cpp

---
+++ cAPS lOCK.cpp
@@ -0,0 +1,72 @@
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+
+
+
+
+#define     pb              push_back
+#define     mp              make_pair
+#define     ff              first
+#define     ss              second
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(int i = 0; i < v.size(); ++i){cout << v[i] << " ";}cout << endl;
+#define     sz(s)           (s).length()
+#define     all(x)          (x).begin(), (x).end()
+#define     digits(x)       trunc(log10(x))+1
+#define     even(x)         (x)%2
+#define     inn             int n;cin>>n;
+
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:/io/input.txt", "r", stdin);
+    freopen("F:/io/output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+    int capital = 0 , small = 0;
+    int len = sz(s);
+    for (int i = 0; i < sz(s); ++i)
+    {
+        if (isupper(s[i]))capital++;
+        else
+            small++;
+    }
+    if((capital == len) || (capital == len-1 && s[0] >= 97))
+    {
+        for (int i = 0; i < len; ++i)
+        {
+            if(islower(s[i]))
+                s[i]=toupper(s[i]);
+            else
+                s[i]=tolower(s[i]);
+        }
+    }
+    
+
+    cout << s;
+
+
+
+    return 0;
+}
