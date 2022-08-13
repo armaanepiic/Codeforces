2aa97a51a2ebcce6857f7daf430db519a3affa00
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-08-13 20:20:26 +0600

    Create Sum of Digits.cpp

---
+++ Sum of Digits.cpp
@@ -0,0 +1,70 @@
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+
+
+
+
+#define     pb              push_back
+#define     mp              make_pair
+#define     ff              first
+#define     ss              second
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(int i = 0; i < v.size(); ++i){cout << v[i] << " ";}cout << endl;
+#define     sz(s)           (s).length()
+#define     all(x)          (x).begin(), (x).end()
+#define     digits(x)       trunc(log10(x))+1
+#define     even(x)         (x)%2
+#define     inn             int n;cin>>n;
+
+ll sum_string(string s)
+{
+    ll sum = 0;
+    for (int i = 0; i < sz(s); ++i)
+    {
+        sum += s[i] - 48;
+    }
+
+    return sum;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:/io/input.txt", "r", stdin);
+    freopen("F:/io/output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+    ll sum , count = 0;
+    if(sz(s) == 1)
+        cout << 0;
+    else
+    {
+        while(sz(s) > 1)
+        {
+            sum = sum_string(s);
+            s = to_string(sum);
+            count++;
+        }
+        cout << count;
+    }
+
+    return 0;
+}
