f2feaa56d2c14a2d01e6e0013f92f4e110f41d6e
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-08-21 02:27:15 +0600

    Create Shovels and Swords.cpp

---
+++ Shovels and Swords.cpp
@@ -0,0 +1,74 @@
+// https://codeforces.com/contest/1366/problem/A
+
+
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+using vvi   =   vector<vector<int>>;
+
+
+
+
+#define     pb              push_back
+#define     mp              make_pair
+#define     ff              first
+#define     ss              second
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(int i = 0; i < v.size(); ++i){cout << v[i] << " ";}cout << endl;
+#define     sz(s)           (s).length()
+#define     all(x)          (x).begin(), (x).end()
+#define     digits(x)       trunc(log10(x))+1
+#define     even(x)         (x)%2
+#define     inn             int n;cin>>n;
+
+
+
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:/io/input.txt", "r", stdin);
+    freopen("F:/io/output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin >> t;
+    while (t--) {
+
+        ll a , b;
+        cin >> a >> b;
+        if(a==0 || b==0)
+            cout << 0 << endl;
+        else{
+            if(a < b)
+                swap(a , b);
+            ll x = a/2;
+            if(b <= x)
+                cout << b << endl;
+            else
+                cout << (a+b)/3 << endl;
+        }
+
+
+    }
+// 100000000
+
+
+    return 0;
+}
