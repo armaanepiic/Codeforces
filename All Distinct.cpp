cf986b54d0ebca95e8f5b9448d0e4e2adc993f73
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-09-06 00:52:17 +0600

    Create All Distinct.cpp

---
+++ All Distinct.cpp
@@ -0,0 +1,82 @@
+// https://codeforces.com/contest/1692/problem/B
+
+
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+using vvi   =   vector<vector<int>>;
+using vvs   =   vector<vector<string>>;
+
+
+
+
+
+
+#define     pb              push_back
+#define     mp              make_pair
+#define     ff              first
+#define     ss              second
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(auto value:v){cout<<value<<" ";}cout<<endl;
+#define     pout(p)         for(auto value:p){cout<<value.ff<<" "<<value.ss<<endl;}cout<<endl;
+#define     sz(s)           (s).length()
+#define     all(x)          (x).begin(), (x).end()
+#define     digits(x)       trunc(log10(x))+1
+
+
+
+
+
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:input.txt", "r", stdin);
+    freopen("F:output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin >> t;
+    while (t--) {
+    
+        bool flag = true;
+        ll count = 0;
+
+        int n;
+        cin >> n;
+        set<int>s;
+        for (int i = 0; i < n; ++i)
+        {
+            int x;cin>>x;
+            s.insert(x);
+        }
+
+        int size = n - s.size();
+        
+        if(size%2==1)
+            cout << n - (size+1) << endl;
+        else
+            cout << n - size << endl;
+
+    }
+
+
+
+    return 0;
+}
