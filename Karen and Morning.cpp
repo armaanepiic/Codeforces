98b0203869ba9037a21ed53ba00a76b48834f003
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-09-09 13:07:11 +0600

    Create Karen and Morning.cpp

---
+++ Karen and Morning.cpp
@@ -0,0 +1,130 @@
+// https://codeforces.com/contest/816/problem/A
+
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+using vvi   =   vector<vector<int>>;
+using vvs   =   vector<vector<string>>;
+
+
+
+
+
+
+#define     pb              push_back
+#define     mp              make_pair
+#define     ff              first
+#define     ss              second
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(auto value:v){cout<<value<<" ";}cout<<endl;
+#define     pout(p)         for(auto value:p){cout<<value.ff<<" "<<value.ss<<endl;}cout<<endl;
+#define     sz(s)           (s).length()
+#define     all(x)          (x).begin(), (x).end()
+#define     digits(x)       trunc(log10(x))+1
+#define     maxelement(x)   max_element(v.begin(),v.end()) - v.begin()
+#define     minelement(x)   min_element(v.begin(),v.end()) - v.begin()
+
+
+
+int dx[8] = {1, -1, 0,  0, 1,  1, -1, -1};
+int dy[8] = {0,  0, 1, -1, 1, -1,  1, -1};
+
+int palindrome(int x)
+{
+    int temp = 0;
+    temp += (x % 10) * 10;
+    x /= 10;
+    temp += x;
+    return temp;
+}
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:input.txt", "r", stdin);
+    freopen("F:output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+
+    // vector<pair<int , int>> p;
+    // p.pb(make_pair(0 , 0));
+    vector<int>v;
+    
+
+    for (int i = 0; i < 24; ++i)
+    {
+        int temp = palindrome(i);
+        if (temp > 59)
+            continue;
+        else
+        {
+            v.pb((i*60)+temp);
+        }
+    }
+    v.pb(24*60);
+    // for(auto a : v)
+    //     cout << a << endl;
+
+    string s;
+    cin >> s;
+    string a = "" , b = "" , c = "";
+    a += s[0];
+    a += s[1];
+    int h = stoi(a);
+    b += s[3];
+    b += s[4];
+    int m = stoi(b);
+    
+    int time = (h*60) + m;
+    // cout << "time = " << time << endl; 
+
+    for (int i = 0; i < v.size(); ++i)
+    {
+        if(v[i] == time){
+            cout << 0 << endl;
+            return 0;
+        }
+        else if(v[i] > time){
+            //cout << v[i] << endl;
+            cout << v[i] - time;
+            break;
+        }
+    }
+// 0
+// 70
+// 140
+// 210
+// 280
+// 350
+// 420
+// 601
+// 671
+// 741
+// 811
+// 881
+// 951
+// 1202
+// 1272
+// 1342
+// 1412
+// 1440
+
+    return 0;
+}
