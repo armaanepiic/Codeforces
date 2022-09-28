1c0c55728d36c1b867ce9b14ce8214d4e41d1d4e
Arman Hossain <73537228+armaanepiic@users.noreply.github.com>
2022-09-28 14:54:27 +0600

    Create A. Elephant.cpp

---
+++ A. Elephant.cpp
@@ -0,0 +1,65 @@
+//Bismillahir rahmanir rahim
+//Arman Hossain C193033
+//Programming is nothing but an addiction.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll    =   long long int;
+using ull   =   unsigned long long;
+using vi    =   vector<int>;
+using vll   =   vector<ll>;
+using vc    =   vector<char>;
+using vs    =   vector<string>;
+using vvi   =   vector<vector<int>>;
+using vvs   =   vector<vector<string>>;
+
+
+
+
+
+
+#define     pb              push_back
+#define     mp              make_pair
+#define     ff              first
+#define     ss              second
+#define     sortv           sort(v.begin(), v.end())
+#define     rev_sortv       sort(v.begin(), v.end(), greater<int>())
+#define     inv             vi v;for(int i = 0; i < n; ++i){int x;cin >> x; v.pb(x);};
+#define     out(v)          for(auto value:v){cout<<value<<" ";}cout<<endl;
+#define     pout(p)         for(auto value:p){cout<<value.ff<<" "<<value.ss<<endl;}cout<<endl;
+#define     sz(s)           (s).length()
+#define     all(x)          (x).begin(), (x).end()
+#define     digits(x)       trunc(log10(x))+1
+#define     maxelement(x)   max_element(v.begin(),v.end()) - v.begin()
+#define     minelement(x)   min_element(v.begin(),v.end()) - v.begin()
+// string to int stoi(x)
+// int/double to string to_string(x)
+
+
+int dx[8] = {1, -1, 0,  0, 1,  1, -1, -1};
+int dy[8] = {0,  0, 1, -1, 1, -1,  1, -1};
+string alpha = " abcdefghijklmnopqrstuvwxyz";
+
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("F:input.txt", "r", stdin);
+    freopen("F:output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+
+    if(n%5==0)
+        cout << n/5;
+    else
+        cout << n/5 + 1;
+    
+}
